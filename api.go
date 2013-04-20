819a433095554ffbabbfc314e2dfaaf54e572bf3
Sergey Gerasimov <mail@grsmv.com>
2013-04-21 00:51:58 +0300

    Tooset's methods moved to api.go

--- api.go
+++ api.go
@@ -55,4 +55,21 @@ func Run (port int) {
     http.ListenAndServe(":" + strconv.Itoa(port), nil)
 }
 
+
+func SetHeader (key, value string) {
+    Context().ResponseWriter.Header().Set(key, value)
+}
+
+
+func SetCookie (key, value string) {}
+
+
+func DeleteCookie (key string) {}
+
+
+func Cookies () map[string]string {
+  cookies := make(map[string]string)
+  return cookies
+}
+
 // vim: noai:ts=4:sw=4
