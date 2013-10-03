1f112cdcaef6c5d77bec38b62f51ac25ad6f47fd
Sergey Gerasimov <mail@grsmv.com>
2013-10-03 19:18:53 +0300

    Using standard listenAndServe instead grace one

--- core/run.go
+++ core/run.go
@@ -1,22 +1,19 @@
 package core
 
 import (
-    "github.com/cliohq/clio/vendor/go.grace/gracehttp"
     "net/http"
     "strconv"
 )
 
-func requestHandler (settings map[string]interface{}) http.Handler {
-
-    mux := http.NewServeMux()
+func requestHandler (settings map[string]interface{}) {
 
     serveStatic := settings["manage-assets"].(bool)
     if serveStatic {
         fs := http.FileServer(http.Dir("public"))
-        mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+        http.Handle("/assets/", http.StripPrefix("/assets/", fs))
     }
 
-    mux.HandleFunc("/", func (w http.ResponseWriter, req *http.Request) {
+    http.HandleFunc("/", func (w http.ResponseWriter, req *http.Request) {
 
         // setting up package variable to use outside the package
         ctx = context { ResponseWriter: w, Request: req }
@@ -26,16 +23,14 @@ func requestHandler (settings map[string]interface{}) http.Handler {
 
         router (w, req)
     })
-    return mux
 }
 
 
 func Run (settings map[string]interface {}) {
-    port := strconv.Itoa(settings["port"].(int))
+    requestHandler (settings)
 
-    gracehttp.Serve (
-        &http.Server { Addr: ":" + port, Handler: requestHandler (settings) },
-    )
+    port := strconv.Itoa(settings["port"].(int))
+    http.ListenAndServe (":" + port, nil)
 }
 
 
