11bd3304b9d1a318c2aa1a8810cf30cef225d73e
Sergey Gerasimov <mail@grsmv.com>
2013-10-16 15:08:45 +0300

    Help documentation updated

--- cli/help.go
+++ cli/help.go
@@ -6,17 +6,25 @@ import (
 
 func Help() {
     fmt.Println (
-        "Usage:                                                        \n" +
-        "   clio COMMAND [FLAGS]                                       \n" +
-        "                                                              \n" +
-        "Commads:                                                      \n" +
-        "   create      Creating application skeleton                  \n" +
-        "   g           Generating scaffold, model, router or view     \n" +
-        "   run         Running application and all helping workers    \n" +
-        "   help        Output this message again                      \n" +
-        "                                                              \n" +
-        "Generators:                                                   \n" +
-        "   clio g [controller | view | router | scaffold] NAME        \n")
+        "Usage:                                                      \n" +
+        "   clio COMMAND [FLAGS]                                     \n" +
+        "                                                            \n" +
+        "Commads:                                                    \n" +
+        "   create      Creating application skeleton                \n" +
+        "   g           Generating scaffold, model, router or view   \n" +
+        "   run         Running application and all helping workers  \n" +
+        "   help        Output this message again                    \n" +
+        "                                                            \n" +
+        "Generators:                                                 \n" +
+        "   clio g [controller | view | router | scaffold] NAME      \n" +
+        "                                                            \n" +
+        "Example:                                                    \n" +
+        "   clio create awesome-application                          \n" +
+        "                                                            \n" +
+        "   This will generate a skeletal Clio application.          \n" +
+        "   Please, see the README in the newly created application  \n" +
+        "   get going. Also http://cliohq.github.io is a good place  \n" +
+        "   for gettingmore information about Clio. Good luck!")
 }
 
 
