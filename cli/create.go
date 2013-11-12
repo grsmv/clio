cb129c117b760ccebf8780b6ffa1bb7df9363986
Sergey Gerasimov <mail@grsmv.com>
2013-11-12 21:55:32 +0200

    issue #48. Creating new application sceleton in GOPATH

--- cli/create.go
+++ cli/create.go
@@ -13,22 +13,58 @@ type Application struct {
     name string
 }
 
+var (
+    GOPATH = os.Getenv ("GOPATH")
+    slash = string (os.PathSeparator)
+    GOPATH_SRC = GOPATH + slash + "src" + slash
+)
+
 
 /**
  *  High level abstraction for creating new app
  *  (called from cli.Route())
  */
 func Create (appName string) {
+
+    checkGopath ()
+    checkContainer (appName)
+
     app := Application { name: appName }
 
     app.createContainer ()
+
     err := app.copyFileTree (
+        // source
         strings.Join (
-            []string { os.Getenv("GOPATH"), applicationTemplatesPath },
-            string (os.PathSeparator),
-        ), app.name)
+            []string { GOPATH, applicationTemplatesPath },
+            slash),
+
+        // destination
+        GOPATH_SRC + app.name)
+
     if err != nil {
         log.Fatal (err)
+    }
+}
+
+
+/**
+ * Cheking if GOPATH setted properly
+ */
+func checkGopath () {
+    if len (GOPATH) == 0 {
+        log.Fatal ("GOPATH is empty. Please fix this")
+    }
+}
+
+
+/**
+ * Checing if folder with desirable name already exists
+ */
+func checkContainer (appName string) {
+    destination := GOPATH_SRC + appName
+    _, err := os.Stat (destination); if os.IsExist (err) {
+        log.Fatal ("folder " + destination + " already exists")
     }
 }
 
@@ -37,14 +73,18 @@ func Create (appName string) {
  *  Creating root folder for new application
  */
 func (app *Application) createContainer () {
-    err := os.Mkdir(app.name, 0776); if err == nil {
-        fmt.Println(green, "  create:", reset, app.name)
+    appPath := GOPATH_SRC + app.name
+    err := os.Mkdir(appPath, 0776); if err == nil {
+        fmt.Println(green, "  create:", reset, appPath)
     } else {
         log.Fatal (err)
     }
 }
 
 
+/**
+ * Creating an application sceleton from templates/application
+ */
 func (app *Application) copyFileTree (from, to string) error {
     file, err := os.Open (from); if err != nil {
         return err
@@ -54,8 +94,8 @@ func (app *Application) copyFileTree (from, to string) error {
     }
     for _, f := range files {
 
-        fromFilePath := strings.Join ([]string { from, f.Name () }, string(os.PathSeparator))
-        toFilePath := strings.Join ([]string { to, f.Name () }, string(os.PathSeparator))
+        fromFilePath := strings.Join ([]string { from, f.Name () }, slash)
+        toFilePath := strings.Join ([]string { to, f.Name () }, slash)
 
         if f.IsDir () == true {
 
@@ -67,7 +107,7 @@ func (app *Application) copyFileTree (from, to string) error {
             }
 
             // scanning next level
-            newTo := strings.Join ([]string { to, f.Name () }, string (os.PathSeparator))
+            newTo := strings.Join ([]string { to, f.Name () }, slash)
             err = app.copyFileTree (fromFilePath, newTo); if err != nil {
                 return err
             }
