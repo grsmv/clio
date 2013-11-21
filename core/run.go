b6af55e354799de381d0f10e54d78b2e307063ed
Sergey Gerasimov <mail@grsmv.com>
2013-11-21 21:36:00 +0200

    functions from clio.go moved to run.go

--- core/run.go
+++ core/run.go
@@ -1,10 +1,37 @@
 package core
 
 import (
+    "fmt"
+    "github.com/cliohq/clio/helpers"
+    "log"
     "net/http"
     "strconv"
-    "log"
 )
+
+var (
+    AppSettings = make(map[string]interface{})
+    routes      = make(map[string] map[string] func () string)
+    splat       = []string{}
+    params      = make(map[string]string)
+    ctx         = context {}
+)
+
+type context struct {
+    Request *http.Request
+    ResponseWriter http.ResponseWriter
+}
+
+
+/**
+ *  Creating namespace to place routes by specific http method
+ */
+func init () {
+    methods := []string{"GET", "POST", "PUT", "DELETE"}
+    for index := range methods {
+        routes[methods[index]] = make (map[string] func () string)
+    }
+}
+
 
 func requestHandler (settings map[string]interface{}) {
 
@@ -43,6 +70,41 @@ func Run (settings map[string]interface {}) {
 }
 
 
-// todo: share settings by IPC
+/**
+ *  Finding correct handler to certain method:path
+ */
+func Router (w http.ResponseWriter, req *http.Request) {
+
+    // splitting whole path into parts
+    path, paramsString := helpers.SplitPath(req.URL.String())
+    routeFound := false
+
+    // finding correct handler
+    for rawPattern, _ := range routes[req.Method] {
+        pattern := helpers.PreparePattern(rawPattern)
+
+        if pattern.MatchString(path) {
+            routeFound = true
+
+            // homage to Sinatra's splat
+            splat = pattern.FindAllStringSubmatch(path, 100)[0][1:]
+
+            // filling params
+            params = helpers.ParseParams(paramsString)
+
+            // calling matched handler
+            fmt.Fprintln(w, routes[req.Method][rawPattern]())
+
+            // terminal debugging
+            if AppSettings["verbose-output"] != nil && AppSettings["verbose-output"].(bool) == true {
+                log.Printf ("%s %s\n", req.Method, req.URL.String())
+            }
+            break
+        }
+    }
+    if !routeFound {
+        NotFound(w, req)
+    }
+}
 
 // vim: noai:ts=4:sw=4
